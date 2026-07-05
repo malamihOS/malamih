be7e173e704fb85ac726b4f8ef9fbcad915875d0
Malamih Creative <satarsteam@gmail.com>
2026-07-06 00:34:06 +0300

    Fix Railway hostname binding

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { parse } = require("url");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = process.env.HOSTNAME || "0.0.0.0";
+const hostname = "0.0.0.0";
 const port = parseInt(process.env.PORT || "3000", 10);
 
 const app = next({ dev, hostname, port, dir: __dirname });
